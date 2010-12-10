91c329ec7f715c02aa0c899e86e24e0e7f206da1
Matthias Tretter <matthias.tretter@gmail.com>
2010-12-11 00:49:57 +0100

    added credits

--- MTStatusBarOverlay.h
+++ MTStatusBarOverlay.h
@@ -6,6 +6,10 @@
 //  Copyright 2010 YellowSoft. All rights reserved.
 //
 
+// Credits go to: 
+// http://stackoverflow.com/questions/2833724/adding-view-on-statusbar-in-iphone
+// http://cocoabyss.com/2010/ios-custom-status-bar/
+
 #import <Foundation/Foundation.h>
 
 
